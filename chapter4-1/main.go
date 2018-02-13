596a5f0e859f33386cf731a54396fe171b0decc9
Paradorn Suksingha <nhumdorn@gmail.com>
2018-02-13 10:09:45 +0700

    Add Chapter 4-1, 5-1, 5-2

--- chapter4-1/main.go
+++ chapter4-1/main.go
@@ -4,26 +4,25 @@ import "fmt"
 
 func main() {
 	// Long Declaration
-	
+
 	//fmt.Println(x)
 
 	// Short Declaration
 	// Type Inference
 	z := "Hello, World"
 
-
 	const x string = "Hello, World"
 	// x = "Other string"
 
 	fmt.Println(z)
-	fmt.Printf("Type: %T %T\n", z,x)
+	fmt.Printf("Type: %T %T\n", z, x)
 
 	var (
 		a = 5
 		b = 10
 		c = 15
 	)
-	fmt.Printf("Type: %T %T %T\n", a,b,c)
+	fmt.Printf("Type: %T %T %T\n", a, b, c)
 
 	v1, v2 := "first", "sec"
 	v1, v2 = v2, v1
