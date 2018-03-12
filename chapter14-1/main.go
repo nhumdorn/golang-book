8a048aa78c2d352c9e391579fa8a4e69383b52a3
Paradorn Suksingha <nhumdorn@gmail.com>
2018-03-12 10:36:02 +0700

    chapter 14-2

--- chapter14-1/main.go
+++ chapter14-1/main.go
@@ -5,15 +5,21 @@ import (
 	"fmt"
 )
 
+type HomePageHandler struct{}
+
+func (h *HomePageHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, World!")
+}
+
 func main() {
 	http.HandleFunc(
 		"/",
 		func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintln(w, "Hello, World!")
 		})
-	barHandler := func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello Bar!")
-	}
-	http.HandleFunc("/bar", barHandler)
+	//barHandler := func(w http.ResponseWriter, r *http.Request) {
+	//	fmt.Fprintln(w, "Hello Bar!")
+	//}
+	http.Handle("/bar", &HomePageHandler{})
 	http.ListenAndServe(":3000", nil)
 }
